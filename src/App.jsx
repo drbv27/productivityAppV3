e3f8c766e70cd55f354af5f98c1e9dca003b12cb
Diego Bonilla <drbv27@gmail.com>
2022-08-25 06:28:02 -0500

    loging page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
 import {ThemeProvider} from './context/ThemeContext'
+import Home from './routes/Home';
+import SignIn from './routes/SignIn';
+import SignUp from './routes/SignUp';
+import Dashboard from './routes/Dashboard';
+import Account from './routes/Account';
 
 import './App.css'
 import Navbar from './components/Navbar'
@@ -10,6 +16,13 @@ function App() {
   return (
     <ThemeProvider>
       <Navbar/>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/dash' element={<Dashboard />} />
+        </Routes>
       <Footer/>
     </ThemeProvider>
   )
