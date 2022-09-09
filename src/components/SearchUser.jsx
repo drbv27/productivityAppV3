211488165b120ffc297904d6ddd3bbaf6502fe32
Diego Bonilla <drbv27@gmail.com>
2022-09-09 10:51:32 -0500

    filter by user in list

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,25 +1,53 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {BiPlayCircle,BiStopCircle,BiUpload,BiSearchAlt,BiPencil} from 'react-icons/bi'
 
 const SearchUser = (usuariosD) => {
+    const [actualUser,setActualUser] = useState({})
     const users = usuariosD.usuariosD
     console.log("usuarios",users)
+
+    async function userFilter(e){
+        e.preventDefault();
+        const userFiltered = e.target.formUsers.value;
+        console.log("detalles",users.filter((userDetail)=>userDetail.details.email===userFiltered));
+        console.log(userFiltered);
+    }
   return (
-    <div className='my-2 w-full relative rounded-2xl shadow-xl'>
+    <>
+    <form onSubmit={userFilter} className='my-2 w-full relative'>
+    <div className='my-2 w-full relative rounded-2xl shadow-xl mb-4'>
     <input 
-      list="users" 
-      name="usuario" 
+      list="usersF" 
+      name="user" 
       id="formUsers" 
       className='w-full p-2 bg-primary border border-input rounded-2xl' 
       placeholder='Buscar Usuario' 
     />
-    <datalist id="users">
+    <datalist id="usersF">
       {users.map((usuario,index) => (
-        <option key={index}>{usuario.details.name} {usuario.details.lastname}</option>
+        <option key={index} value={usuario.details.email} >{usuario.details.name} {usuario.details.lastname}</option>
       ))}
     </datalist>
       <BiSearchAlt className='absolute right-2 top-3 text-gray-400 text-2xl' />
   </div>
+
+  <div className='flex justify-between gap-2'>
+    <input type="date" name="" id="" className='grow'/>
+    <input type="date" name="" id="" className='grow'/>
+    <button>Consultar</button>
+  </div>
+  </form>
+  <div>
+  <div className="flex">
+        <div>
+            <h3>Nombre:</h3>
+        </div>
+        <div>Grafica</div>
+    </div>
+            <div>tabla</div>
+  </div>
+
+  </>
   )
 }
 
--- src/routes/UsersPanel.jsx
+++ src/routes/UsersPanel.jsx
@@ -12,6 +12,7 @@ import {
   } from "firebase/firestore";
 import Navbar from "../components/Navbar";
 import SearchUser from "../components/SearchUser";
+import Edi from "../assets/img/EDI_RELAJADO.png"
 
 
 const firestore = getFirestore(app);
@@ -45,12 +46,13 @@ const UsersPanel =  ()=>{
     return (
         <>
         <Navbar/>
-        <h2>Prueba</h2>
-        <div className='max-w-[1140px] mx-auto'>
-            <div className='bg-secondary flex flex-col justfiy-between items-center my-5 py-8 px-4 rounded-xl'>
+        <fieldset className='bg-secondary py-5 px-2 rounded-2xl shadow-xl mx-16 mt-10'>
+        <legend className='text-3xl flex'>Usuarios  <img src={Edi} alt="" className='ml-2 w-16'/></legend>
+            <div className='bg-secondary flex flex-col justfiy-between items-center my-5 py-2 px-4 rounded-xl'>
                 <SearchUser usuariosD={usuariosD}/>
             </div>
-        </div>
+   
+        </fieldset>
 
         </>
     )
